647aed6ed09cfe4335ed21ca0c7fb056e931c06e
Jordiii <jordiandersen@hotmail.com>
2015-06-10 18:28:15 +0200

    d

--- MidnFeed - By Jordiii and.lua
+++ MidnFeed - By Jordiii and.lua
@@ -5,7 +5,11 @@ end
 
 function OnTick()
 	if FeedConfig.enabled then
-		print("MidnFeed Succesfully Loaded! Enjoy your game. note that feeding is griefing, you may or may not get banned") 
+		if _loaded == nil then _loaded = false end
+		if _loaded == false then
+			print("MidnFeed Succesfully Loaded! Enjoy your game. note that feeding is griefing, you may or may not get banned") 
+			_loaded = true
+		end
 		if myHero.team == 100 then myHero:MoveTo(25000,25000) else myHero:MoveTo(0,0) end CastSpell(4) CastSpell(5) 
 	end
 end
