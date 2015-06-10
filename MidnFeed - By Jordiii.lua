c660f21240d97f13529b7d06cea653435636778c
Jordiii <jordiandersen@hotmail.com>
2015-06-10 18:31:44 +0200

    MidnFeed - By Jordiii
    Burn helped alot thanks:D

---
+++ MidnFeed - By Jordiii.lua
@@ -0,0 +1,15 @@
+function OnLoad()	
+	FeedConfig = scriptConfig("Feed Script", "feeder")
+	FeedConfig:addParam("enabled", "Enable Feed", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("0"))
+end
+
+function OnTick()
+	if FeedConfig.enabled then
+		if _loaded == nil then _loaded = false end
+		if _loaded == false then
+			print("MidnFeed Succesfully Loaded! Enjoy your game. note that feeding is griefing, you may or may not get banned") 
+			_loaded = true
+		end
+		if myHero.team == 100 then myHero:MoveTo(25000,25000) else myHero:MoveTo(0,0) end CastSpell(4) CastSpell(5) 
+	end
+end
